51260fff71ffeedae14a2d9495ef08f7cc1634f1
R Sánchez <rene.as.himself@gmail.com>
2019-01-01 00:26:58 -0600

    Cleaned unused CORS code

--- server.js
+++ server.js
@@ -26,17 +26,6 @@ app.use(bodyParser.json())
 
 // app.use(cors())
 
-// app.use(function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Credentials', true)
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-//     res.header(
-//         'Access-Control-Allow-Headers',
-//         'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-//     )
-//     next()
-// })
-
 app.use(function(req, res, next) {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*')
